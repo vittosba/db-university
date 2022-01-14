b46acb7a5d0a80eb67b1ea58a4732050dd44b09b
vittosba <vitsbardella@gmail.com>
2022-01-14 15:43:13 +0100

    sql parte 2

---
+++ Sql-Query-parte-2.sql
@@ -0,0 +1,51 @@
+-- JOIN
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * 
+FROM `students` 
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT * 
+FROM `departments`
+INNER JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT * 
+FROM `courses` 
+INNER JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`id` = 44;
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT * 
+FROM `students`
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
+
+
+-- GROUP BY
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(id), YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(id), `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT AVG(vote) 
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT COUNT(id), `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
