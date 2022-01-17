486e8df33de60a79b4fd083a74e7cdf017ade330
vittosba <vitsbardella@gmail.com>
2022-01-17 15:40:44 +0100

    correzione-pt2

--- Sql-Query-parte-2.sql
+++ Sql-Query-parte-2.sql
@@ -31,9 +31,9 @@ ORDER BY `students`.`surname`, `students`.`name`;
 -- GROUP BY
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-SELECT COUNT(id), YEAR(`enrolment_date`)
+SELECT COUNT(id), YEAR(`enrolment_date`) AS `anno`
 FROM `students`
-GROUP BY YEAR(`enrolment_date`);
+GROUP BY `anno`
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT COUNT(id), `office_address`
